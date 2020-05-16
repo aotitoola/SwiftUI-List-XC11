deac6a630a57797ab78eb319ddbfc0c0a0b767ba
Aanuoluwa Otitoola <aanuoluwa.otitoola@gmail.com>
2020-05-16 16:37:54 +0100

    Initial Commit

---
+++ SwiftUI-List-XC11/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-List-XC11
+//
+//  Created by Aanuoluwa Otitoola on 16/05/2020.
+//  Copyright © 2020 Aanuoluwa Otitoola. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
